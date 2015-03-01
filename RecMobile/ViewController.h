878309983bf357ab4b96b1975d9cf42a3dd89df1
BiXiaopeng <bixiaopeng2007@gmail.com>
2015-03-01 11:45:16 +0900

    Initial Commit

---
+++ RecMobile/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RecMobile
+//
+//  Created by BiXiaopeng on 15/3/1.
+//  Copyright (c) 2015年 BiXiaopeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RecMobile/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RecMobile
+//
+//  Created by BiXiaopeng on 15/3/1.
+//  Copyright (c) 2015年 BiXiaopeng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
